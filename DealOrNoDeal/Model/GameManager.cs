75154a3c33f62b0c7cd361c48ceffebfd8077329
Nicolas Sacandy <nicsacandy@gmail.com>
2024-05-12 15:35:44 -0400

    Add project files.

---
+++ DealOrNoDeal/Model/GameManager.cs
@@ -0,0 +1,51 @@
+﻿namespace DealOrNoDeal.Model
+{
+    /// <summary>Handles the management of the actual game play.</summary>
+    public class GameManager
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameManager"/> class.
+        /// </summary>
+        public GameManager()
+        {
+
+        }
+
+        /// <summary>
+        ///     Removes the specified briefcase from play.
+        ///     Precondition: None
+        /// </summary>
+        /// <param name="id">The id of the briefcase to remove from play.</param>
+        /// <returns>Dollar amount stored in the case, or -1 if case not found.</returns>
+        public int RemoveBriefcaseFromPlay(int id)
+        {
+            // TODO Complete method according to the specifications above
+            return -1;
+        }
+
+        /// <summary>
+        ///     TODO Complete method specification
+        /// </summary>
+        /// <returns></returns>
+        public int GetOffer()
+        {
+            // TODO Collaborates with the banker class to get and return the amount of the offer after the round has completed.
+            //      Note: You will need to figure out the number of cases to open in the next round and once you have
+            //
+            return -1;
+        }
+
+        /// <summary>
+        ///     Moves to next round by incrementing Round property and setting
+        ///     initial number of cases for that round
+        ///     Precondition: None
+        ///     Postcondition: Round == Round@prev + 1 AND CasesRemainingInRound == (number of cases to open in the next round)
+        /// </summary>
+        public void MoveToNextRound()
+        {
+            // TODO Complete this method according to its specification
+        }
+
+    }
+}
---
+++ DealOrNoDeal/View/DealOrNoDealPage.xaml.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DealOrNoDeal.View
+{
+    /// <summary>
+    ///     An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DealOrNoDealPage
+    {
+        #region Data members
+
+        /// <summary>
+        ///     The application height
+        /// </summary>
+        public const int ApplicationHeight = 500;
+
+        /// <summary>
+        ///     The application width
+        /// </summary>
+        public const int ApplicationWidth = 500;
+
+        private IList<Button> briefcaseButtons;
+        private IList<Border> dollarAmountLabels;
+
+        #endregion
+
+        #region Constructors
+
+        public DealOrNoDealPage()
+        {
+            this.InitializeComponent();
+            this.initializeUiDataAndControls();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void initializeUiDataAndControls()
+        {
+            this.setPageSize();
+
+            this.briefcaseButtons = new List<Button>();
+            this.dollarAmountLabels = new List<Border>();
+            this.buildBriefcaseButtonCollection();
+            this.buildDollarAmountLabelCollection();
+        }
+
+        private void setPageSize()
+        {
+            ApplicationView.PreferredLaunchViewSize = new Size {Width = ApplicationWidth, Height = ApplicationHeight};
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(ApplicationWidth, ApplicationHeight));
+        }
+
+        private void buildDollarAmountLabelCollection()
+        {
+            this.dollarAmountLabels.Clear();
+
+            this.dollarAmountLabels.Add(this.label0Border);
+            this.dollarAmountLabels.Add(this.label1Border);
+            this.dollarAmountLabels.Add(this.label2Border);
+            this.dollarAmountLabels.Add(this.label3Border);
+            this.dollarAmountLabels.Add(this.label4Border);
+            this.dollarAmountLabels.Add(this.label5Border);
+            this.dollarAmountLabels.Add(this.label6Border);
+            this.dollarAmountLabels.Add(this.label7Border);
+            this.dollarAmountLabels.Add(this.label8Border);
+            this.dollarAmountLabels.Add(this.label9Border);
+            this.dollarAmountLabels.Add(this.label10Border);
+            this.dollarAmountLabels.Add(this.label11Border);
+            this.dollarAmountLabels.Add(this.label12Border);
+            this.dollarAmountLabels.Add(this.label13Border);
+            this.dollarAmountLabels.Add(this.label14Border);
+            this.dollarAmountLabels.Add(this.label15Border);
+            this.dollarAmountLabels.Add(this.label16Border);
+            this.dollarAmountLabels.Add(this.label17Border);
+            this.dollarAmountLabels.Add(this.label18Border);
+            this.dollarAmountLabels.Add(this.label19Border);
+            this.dollarAmountLabels.Add(this.label20Border);
+            this.dollarAmountLabels.Add(this.label21Border);
+            this.dollarAmountLabels.Add(this.label22Border);
+            this.dollarAmountLabels.Add(this.label23Border);
+            this.dollarAmountLabels.Add(this.label24Border);
+            this.dollarAmountLabels.Add(this.label25Border);
+        }
+
+        private void buildBriefcaseButtonCollection()
+        {
+            this.briefcaseButtons.Clear();
+
+            this.briefcaseButtons.Add(this.case0);
+            this.briefcaseButtons.Add(this.case1);
+            this.briefcaseButtons.Add(this.case2);
+            this.briefcaseButtons.Add(this.case3);
+            this.briefcaseButtons.Add(this.case4);
+            this.briefcaseButtons.Add(this.case5);
+            this.briefcaseButtons.Add(this.case6);
+            this.briefcaseButtons.Add(this.case7);
+            this.briefcaseButtons.Add(this.case8);
+            this.briefcaseButtons.Add(this.case9);
+            this.briefcaseButtons.Add(this.case10);
+            this.briefcaseButtons.Add(this.case11);
+            this.briefcaseButtons.Add(this.case12);
+            this.briefcaseButtons.Add(this.case13);
+            this.briefcaseButtons.Add(this.case14);
+            this.briefcaseButtons.Add(this.case15);
+            this.briefcaseButtons.Add(this.case16);
+            this.briefcaseButtons.Add(this.case17);
+            this.briefcaseButtons.Add(this.case18);
+            this.briefcaseButtons.Add(this.case19);
+            this.briefcaseButtons.Add(this.case20);
+            this.briefcaseButtons.Add(this.case21);
+            this.briefcaseButtons.Add(this.case22);
+            this.briefcaseButtons.Add(this.case23);
+            this.briefcaseButtons.Add(this.case24);
+            this.briefcaseButtons.Add(this.case25);
+
+            this.storeBriefCaseIndexInControlsTagProperty();
+        }
+
+        private void storeBriefCaseIndexInControlsTagProperty()
+        {
+            for (var i = 0; i < this.briefcaseButtons.Count; i++)
+            {
+                this.briefcaseButtons[i].Tag = i;
+            }
+        }
+
+        private void briefcase_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO The sender variable holds the button object that was clicked;
+            // assign the sender variable to a variable of type Button
+
+            // TODO Set the button (briefcase) to not be enabled and its visibility to collapsed
+
+            // TODO Get the ID of the briefcase that was clicked (hint: use the getBriefCaseID method)
+
+            // TODO Call GameManager::RemoveBriefCaseFromPlay to remove the brief case from the current cases in play
+            // Gray out the corresponding game amount label
+        }
+
+        private void findAndGrayOutGameDollarLabel(int amount)
+        {
+            foreach (var currDollarAmountLabel in this.dollarAmountLabels)
+            {
+                if (grayOutLabelIfMatchesDollarAmount(amount, currDollarAmountLabel))
+                {
+                    break;
+                }
+            }
+        }
+
+        private static bool grayOutLabelIfMatchesDollarAmount(int amount, Border currDollarAmountLabel)
+        {
+            var matched = false;
+
+            if (currDollarAmountLabel.Child is TextBlock dollarTextBlock)
+            {
+                var labelAmount = int.Parse(dollarTextBlock.Text, NumberStyles.Currency);
+                if (labelAmount == amount)
+                {
+                    currDollarAmountLabel.Background = new SolidColorBrush(Colors.Gray);
+                    matched = true;
+                }
+            }
+
+            return matched;
+        }
+
+        private int getBriefcaseID(Button selectedBriefCase)
+        {
+            // TODO return the integer value (ID) stored in the Button's Tag property.
+            // HINT A type cast will be needed
+            return -1;
+        }
+
+        private void updateCurrentRoundInformation()
+        {
+            // TODO This method will need to update the text for the information labels
+            //       to display the current round and cases to open, as well as, the number of cases
+            //       left to open for this round
+
+            // TODO If a round is complete, then collaborate with the GameManager to get the banker's offer and display the appropriate text in the summaryOutput
+        }
+
+        private void dealButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO Complete this method to output the information when the player accepts the Banker's deal
+            // Check for instance of last round, as in the last round the deal button
+            // displays the player's case value
+        }
+
+        private void noDealButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO Handle the the selection of a case in the final round
+            // You may not want to worry about the final round code until this
+            // code can successfully move onto the next round
+
+            // TODO Otherwise, move to the next round (hint: method in GameManager class)
+            // and update summaryOutput information and the label information appropriately
+            // You will also need to handle the situation when advancing to the next round and the
+            // next round is the final round, so the text on the Deal and No Deal buttons will 
+            // need to change as detailed in the specifications
+        }
+
+        #endregion
+    }
+}
